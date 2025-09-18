98f14feba55f818fbbd7a33c96cfa0dffa7bddcb
LAHCEN BIL <billlhssn@gmail.com>
2025-09-18 16:47:11 +0100

    docs: add inline comments for favorites and recommendations logic in Zustand store

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -5,6 +5,8 @@ import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
                 <h2>Recipes</h2>
                 <SearchBar />
                 <RecipeList />
+                <FavoritesList />
+                <RecommendationsList />
               </>
             }
           />
