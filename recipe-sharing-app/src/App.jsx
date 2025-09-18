1cb77f6504b5ce06e648fdff6ca239fea5dc2dd0
LAHCEN BIL <billlhssn@gmail.com>
2025-09-18 16:42:42 +0100

    feat: add advanced search and filtering to recipe app (Zustand store, SearchBar, RecipeList, UI integration)

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
               <>
                 <AddRecipeForm />
                 <h2>Recipes</h2>
+                <SearchBar />
                 <RecipeList />
               </>
             }
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,10 +2,18 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
   addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   deleteRecipe: (id) => set((state) => ({ recipes: state.recipes.filter(recipe => recipe.id !== id) })),
   updateRecipe: (updatedRecipe) => set((state) => ({
     recipes: state.recipes.map(recipe => recipe.id === updatedRecipe.id ? { ...recipe, ...updatedRecipe } : recipe)
   })),
   setRecipes: (recipes) => set({ recipes }),
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filterRecipes: () => set((state) => ({
+    filteredRecipes: state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    )
+  })),
 }));
