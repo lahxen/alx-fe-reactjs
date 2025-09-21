42743b3929b5e54cc1924dc69f9658f86937f6d7
LAHCEN BIL <billlhssn@gmail.com>
2025-09-21 01:13:43 +0000

    Fix: ensure fetchUserData is defined and exported in githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,15 @@
-import axios from 'axios';
 
+import axios from 'axios';
 const BASE_URL = 'https://api.github.com';
 const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
+
+// Fetch user data by username
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`${BASE_URL}/users/${username}`, {
+    headers: GITHUB_API_KEY ? { Authorization: `Bearer ${GITHUB_API_KEY}` } : {},
+  });
+  return response.data;
+};
 
 export const searchUsers = async (username) => {
   const response = await axios.get(`${BASE_URL}/search/users`, {
